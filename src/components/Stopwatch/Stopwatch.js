4711c1691a7b9e76ddc213daf6133a67c92d9ee4
Piotr Węgłowski <weglowskipiotr@gmail.com>
2023-11-04 15:45:51 +0100

    Fix bug in Stopwatch component

--- src/components/Stopwatch/Stopwatch.js
+++ src/components/Stopwatch/Stopwatch.js
@@ -10,6 +10,7 @@ const Stopwatch = ({ time }) => {
     hours = hours < 10 ? '0' + hours : hours;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     seconds = seconds < 10 ? '0' + seconds : seconds;
+    milliseconds = milliseconds < 10 ? '0' + milliseconds : milliseconds;
 
     return hours + ':' + minutes + ':' + seconds + '.' + milliseconds;
   };
